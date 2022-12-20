17564722e9cedd1c379e90f52912015981b09467
H. Vetinari <h.vetinari@gmx.com>
2022-12-20 11:34:24 +1100

    set -D_LIBCPP_DISABLE_AVAILABILITY on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -ex
 
-set -e
-set -x
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
 
 export UNIXODBC_INCLUDE_DIR=$CONDA_PREFIX/include
 python -m pip install -vv .
