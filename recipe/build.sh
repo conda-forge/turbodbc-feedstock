543351701e3a0a965153910068c4528620f4bece
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-19 21:06:00 +0200

    Explicitly set pyarrow-include-dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ set -x
 
 export LDFLAGS="$LDFLAGS -L$SP_DIR/pyarrow"
 export UNIXODBC_INCLUDE_DIR=$CONDA_PREFIX/include
-python -m pip install -vv .
+python -m pip install -vv . --config-settings=setup-args="--pyarrow-include-dir=$SP_DIR/pyarrow/include"
