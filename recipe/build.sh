1ce3599dcb303aa0c56d89f57f2f0680dde032cf
Uwe Korn <uwelk@xhochy.com>
2024-09-26 08:07:52 +0000

    Cleanup recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e
-set -x
+set -euxo pipefail
+
+python -c 'import pyarrow as pa; print(pa.get_include())'
+$PYTHON -c 'import pyarrow as pa; print(pa.get_include())'
 
 export LDFLAGS="$LDFLAGS -L$SP_DIR/pyarrow"
 export UNIXODBC_INCLUDE_DIR=$CONDA_PREFIX/include
-python -m pip install -vv . --config-settings=setup-args="-Dpyarrow-include-dir=$SP_DIR/pyarrow/include"
+python -m pip install -vv .
