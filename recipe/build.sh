e46e8376942ec9b2db364633aa5515fff491da9c
Uwe L. Korn <xhochy@users.noreply.github.com>
2024-10-11 18:37:44 +0200

    Drop outdated pin_compatible('numpy') (#138)
    * Drop outdated pin_compatible('numpy')
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.42.0, and conda-forge-pinning 2024.10.10.05.31.44
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.42.0, and conda-forge-pinning 2024.10.10.08.45.34
    
    * Adapt to new clang/meson activation

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,12 @@ set -exuo pipefail
 
 mkdir -p builddir
 
-echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+if [[ "${target_platform}" != "${build_platform}" ]]; then
+    sed -i "/^\[binaries\]/a python = '${PREFIX}/bin/python'" ${BUILD_PREFIX}/meson_cross_file.txt
+fi
 
 $PYTHON -m build -w -n -x \
     -Cbuilddir=builddir \
     -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
-pip install -vvv dist/turbodbc-*.whl
+$PYTHON -m pip install -vvv dist/turbodbc-*.whl
